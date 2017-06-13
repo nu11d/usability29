2bfb360748b5fbfd8d8074b8a776c73a259633f6
nu11d <nullda@hotmail.com>
2017-06-13 13:25:53 +0900

    return http;

--- module1.js
+++ module1.js
@@ -369,10 +369,10 @@
 	let InMemHeroService = function () {};
 
 	InMemHeroService.prototype.lst = function () {
-		let http = new ng.http;
+		let http = new ng.http.Http;
 		let name = ["Sprite","X","Net","Mix"];
 		//return http.Http.get('./heroes.json');
-		return http.Http;
+		return http;
 	}
 
 	
